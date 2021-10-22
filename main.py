b09472d7c7735c4146388a912b3b03253a33ae6b
Behruz HOME <rihsimovbehruz@gmail.com>
2021-10-22 19:37:23 +0500

    to notebook

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import logging
-
 import aiogram.types
 from aiogram import Bot, Dispatcher, executor, types
 import json
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
 
 API_TOKEN = '1018761895:AAE9zGMHZxYZlC_6kyRLAmTBC0Oubpp-QUQ'
 
@@ -27,20 +27,34 @@ def inline_keyboard_creator(*args, row_width=2):
     :return: Ready InlineKeyboardMarkup
     """
 
-    buttons = []
-    print(args)
+    ready_buttons = InlineKeyboardMarkup(row_width=row_width)
+
     for i in args:
-        print(i)
-        if i[1].startswith('tg://'):
-            button = aiogram.types.InlineKeyboardButton(i[0], url=i[1])
+        if i[1].startswith('https://'):
+            button = InlineKeyboardButton(i[0], url=i[1])
+            ready_buttons.add(button)
         else:
-            button = aiogram.types.InlineKeyboardButton(i[0], callback_data=i[1])
-
-        buttons.append(button)
-
-    ready_buttons = aiogram.types.InlineKeyboardMarkup(row_width=row_width, inline_keyboard=buttons)
+            button = InlineKeyboardButton(i[0], callback_data=i[1])
+            ready_buttons.add(button)
 
     return ready_buttons
+
+
+# Creates ReplyKeyboardCreator
+def reply_keyboard_creator(buttons_list):
+    """
+    :param args: Передаешь массивы, каждый массив это одна строка, в внутри массива будет еще массавы которые каждый
+    :return:
+    """
+
+    ready_buttons = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    for i in buttons_list:
+        row = [KeyboardButton(j) for j in i]
+        ready_buttons.row(*row)
+
+    return ready_buttons
+
 
 
 # Command "/start" handler
@@ -50,15 +64,29 @@ async def send_welcome(message: types.Message):
     await message.answer_photo(photo=open('images/logo.jpg', 'rb'))
 
     # Sends Welcome Message with two inline
-    balance = '0'
-    bot_support = 'tg://iso_support_bot'
-    buttons = inline_keyboard_creator([balance, 'welcome'],
-                                      [bot_mesg['bot_support'], bot_support])
-    gen_inline_keyboard_buttons = inline_keyboard_creator(buttons)
-
+    balance = 0
+    balance_mesg = f'Баланс ({balance})'
+    bot_support = 'https://t.me/iso_support_bot'
+    gen_inline_keyboard_buttons = inline_keyboard_creator([balance_mesg, 'welcome'],
+                                                          [bot_mesg['bot_support'], bot_support])
     await message.answer(bot_mesg['welcome'], parse_mode='html', reply_markup=gen_inline_keyboard_buttons)
 
+    # Good luck and main menu reply keyboards
+    feedback_count = 444
+    buttons = [
+        ['Локации', 'Товары (Уфа)', 'Профиль'],
+        ['Баланс', f'Отзывы ({feedback_count})', 'Поддержка'],
+        ['Заработать']
+    ]
+    ready_buttons = reply_keyboard_creator(buttons)
+    await message.answer('Удачных покупок!', reply_markup=ready_buttons)
 
+    # Do you wan't referral code?  inlinekeyboard: show referral code
+    referral_code = inline_keyboard_creator(['Ввести реферальный код', 'referral_code'])
+    await message.answer('Хотите ввести код реферала? После ввода кода реферала на ваш баланс будет начислено 50 руб',
+                         reply_markup=referral_code)
+
+    choose_cities()
 
 
 if __name__ == "__main__":
